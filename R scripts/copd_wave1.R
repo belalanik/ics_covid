d703dc21c79df49e922713678c4b5793f91c6563
M. Bokern <103196673+bokern@users.noreply.github.com>
2023-08-09 16:21:06 +0100

    Graphs

--- R scripts/copd_wave1.R
+++ R scripts/copd_wave1.R
@@ -28,7 +28,7 @@ surv1 <- Surv(subset_df$timeinstudy1, subset_df$pos_covid_test_present)
 km_curve1 <- survfit(surv1 ~ subset_df$treat, data = subset_df)
 
 # Plot the Kaplan-Meier curve
-ggsurvplot(km_curve1, data = subset_df, conf.int = T, censor = F, ylim = c(0.98, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[8]))
+ggsurvplot(km_curve1, data = subset_df, conf.int = T, censor = F, ylim = c(0.95, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[8]))
 
 
 # OUTCOME: COVID HOSPITALISATION. Generate the Kaplan-Meier curve
@@ -36,14 +36,14 @@ surv2 <- Surv(subset_df$timeinstudy2, subset_df$covid_hes_present)
 km_curve2 <- survfit(surv2 ~ subset_df$treat, data = subset_df)
 
 # Plot the Kaplan-Meier curve
-ggsurvplot(km_curve2, data = subset_df, conf.int = T, censor = F, ylim = c(0.98, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[4]))
+ggsurvplot(km_curve2, data = subset_df, conf.int = T, censor = F, ylim = c(0.95, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[4]))
 
 # OUTCOME: COVID DEATH. Generate the Kaplan-Meier curve
 surv3 <- Surv(subset_df$timeinstudy3, subset_df$covid_death_present) 
 km_curve3 <- survfit(surv3 ~ subset_df$treat, data = subset_df)
 
 # Plot the Kaplan-Meier curve
-ggsurvplot(km_curve3, data = subset_df, conf.int = T, censor = F, ylim = c(0.98, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[7]))
+ggsurvplot(km_curve3, data = subset_df, conf.int = T, censor = F, ylim = c(0.95, 1), xlab = "Time in days", risk.table = TRUE, legend.labs = c("LABA/LAMA group", "ICS group"), legend.title = "", palette = c(palette[1], palette[7]))
 
 
 factor_cols <- c("pos_covid_test_present", "covid_hes_present", "covid_death_present")
@@ -58,6 +58,7 @@ cuminc1 <- cuminc(Surv(timeinstudy1, pos_covid_test_present) ~ treat, data = sub
   ggtitle("Cumulative Incidence of Positive COVID-19 Test by Treatment Group") +
   theme(text = element_text(size = 12))
 ggsave(filename = "cuminc1_pos_covid_test.png", plot = cuminc1, path = Tables, width = 10, height = 6, units = "in", dpi = 300)
+
 
 cuminc2 <- cuminc(Surv(timeinstudy2, covid_hes_present) ~ treat, data = subset_df) %>% 
   ggcuminc(outcome = "1") +
@@ -76,9 +77,13 @@ cuminc3 <- cuminc(Surv(timeinstudy3, covid_death_present) ~ treat, data = subset
 ggsave(filename = "cuminc3_covid_death.png", plot = cuminc3, path = Tables, width = 10, height = 6, units = "in", dpi = 300)
 
 
+# COX REGRESSION ----------------------------------------------------------
+
 cox1 <- coxph(surv1 ~ treat, data = subset_df)
 sch.resid1 = cox.zph(cox1, transform ='identity')
 plot(sch.resid1)
+ tidy(cox1)
+
 
 cox2 <- coxph(surv2 ~ treat, data = subset_df)
 sch.resid2 = cox.zph(cox2, transform ='identity')
@@ -96,6 +101,7 @@ plot(subset_df$age_index, mgale_res1)
 cox2a <- coxph(surv2 ~ treat, data = subset_df)
 
 cox3a <- coxph(surv3 ~ treat, data = subset_df)
+
 
 
 subset_df <- df[!is.na(df$treatgroup),]
